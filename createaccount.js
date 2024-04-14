dad7bc64d38d93f8c5238d1c47e70829ac6b05df
Megan Simkus <150747745+mhsimkus@users.noreply.github.com>
2024-04-14 09:06:05 -0700

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -40,13 +40,13 @@ function CreateAccount(){
         <>
         Name<br/>
         <input type="input" className="form-control" id="name"
-        placeholder="Enter name" value={name} onChange{e =>setName(e.currentTarget.value)} /><br/>
+        placeholder="Enter name" value={name} onChange={e =>setName(e.currentTarget.value)} /><br/>
         Email address<br/>
         <input type="input" className="form-control" id="email"
-        placeholder="Enter email" value={email} onChange{e =>setEmail(e.currentTarget.value)} /><br/>
+        placeholder="Enter email" value={email} onChange={e =>setEmail(e.currentTarget.value)} /><br/>
         Password<br/>
         <input type="input" className="form-control" id="password"
-        placeholder="Enter password" value={password} onChange{e =>setPassword(e.currentTarget.value)} /><br/>
+        placeholder="Enter password" value={password} onChange={e =>setPassword(e.currentTarget.value)} /><br/>
         <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
         </>
        ):(
