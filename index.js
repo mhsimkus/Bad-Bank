dbc97e030633b124464de28e136ece9f4d42dc09
Megan Simkus <150747745+mhsimkus@users.noreply.github.com>
2024-05-13 09:03:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function Spa() {
     return(
       <HashRouter>
       <NavBar/>
-      <UserContext.Provider> value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
         
       <Route path="/" exact component={Home} />
       <Route path="/CreateAccount/" component={CreateAccount} />
